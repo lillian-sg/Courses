9190dcf0c0237bc8cc1ab60aecdf3f835ceee6d5
Lillian-SG <souza.lillian@gmail.com>
2021-05-22 09:49:31 -0300

    Adiciona metódo pra mostrar e esconder load

--- Cursos/Controllers/AddCourseViewController.swift
+++ Cursos/Controllers/AddCourseViewController.swift
@@ -9,12 +9,35 @@ import UIKit
 
 class AddCourseViewController: UIViewController {
 
+    @IBOutlet weak var courseNameTextField: UITextField!
+    
+    @IBOutlet weak var courseLevelTextField: UITextField!
+    
+    @IBOutlet weak var addButton: UIButton!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        showLoad(false)
       
     }
     
-
+    func showLoad(_ show: Bool) {
+        if show {
+            addButton.setTitle("", for: .normal)
+            activityIndicator.startAnimating()
+        }else {
+            let titleButton = "ADICIONAR"
+            addButton.setTitle(titleButton, for: .normal)
+            activityIndicator.stopAnimating()
+        }
+    
+        
+    }
+    
+    @IBAction func addCourse() {
+    }
+    
   
 }
